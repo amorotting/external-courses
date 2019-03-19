e6d71ef73ec11c00fcb96f97983caa0043d82c67
Roman <Roman_Pashkin@epam.com>
2019-03-19 15:11:00 +0300

    Changes: 1-removed extra brackets

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -2,7 +2,7 @@ function typeOf (a) {
 	var result;
 	if (typeof a === "string") {
 		result = "string";
-	} else if ((typeof a === "number") && (isNaN(a) !== true)){
+	} else if (typeof a === "number" && isNaN(a) !== true){
 		result = "number";
 	} else {
 		result = undefined;
